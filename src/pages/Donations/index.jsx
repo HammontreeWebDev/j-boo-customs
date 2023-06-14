ed233c86467667d8d7e7fa8f92d61f2fbeb5a2e0
HammontreeWebDev <hammontreewebdevelopment@gmail.com>
2023-06-13 23:00:45 -0400

    added initial set up of donations page

---
+++ src/pages/Donations/index.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+// Donations Page | route = /donations
+const Donations = () => {
+
+    return(
+        <h1>This is the Donations Page</h1>
+    )
+};
+
+export default Donations;
