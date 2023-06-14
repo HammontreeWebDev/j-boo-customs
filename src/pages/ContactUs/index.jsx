6634e608957d93fca4e911e2b4eb13dc2da78a6e
HammontreeWebDev <hammontreewebdevelopment@gmail.com>
2023-06-13 22:59:58 -0400

    added initial set up of contact us page

---
+++ src/pages/ContactUs/index.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+// Contact Us Page | route = /contactus
+const ContactUs = () => {
+
+    return(
+        <h1>This is the Contact Us Page</h1>
+    )
+};
+
+export default ContactUs;
