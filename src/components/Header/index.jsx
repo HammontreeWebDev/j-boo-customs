e2b4f7f006f318b9128d12ddb048b05a1ec714a3
HammontreeWebDev <hammontreewebdevelopment@gmail.com>
2023-06-14 16:22:58 -0400

    header shape properly sized and scaled

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 
 import './assets/css/header.css';
+import headerShape from './assets/img/header-shape.svg';
 
 const Header = (props) => {
     return(
         <header className="header-component" style={{backgroundImage: `url(${props.backgroundImage})`}}>
+            <img className="header-shape" src={headerShape} />
 
         </header>
     )
