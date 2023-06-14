bfb52a14adf655ab3ff181e3d270551e1b295b55
HammontreeWebDev <hammontreewebdevelopment@gmail.com>
2023-06-13 23:02:09 -0400

    added initial set up of jesse's story page

---
+++ src/pages/JessesStory/index.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+// Jesse's Story Page | route = /jessesstory
+const JessesStory = () => {
+
+    return(
+        <h1>This is the Jesse's Story Page</h1>
+    )
+};
+
+export default JessesStory;
