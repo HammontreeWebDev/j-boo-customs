d62f94c3b3c38258a96300322d40b2ad58656cae
HammontreeWebDev <hammontreewebdevelopment@gmail.com>
2023-06-13 23:26:46 -0400

    added routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 // import all pages for react router to display properly
 import Landing from './pages/Landing';
@@ -11,9 +12,22 @@ import Donations from './pages/Donations';
 // css file
 import './App.css';
 
+// ! only need to tell the router which page to render based on the path associated in the URL | Each Page will dictate A header component (also containing a navbar) that will pass props (images will change on each page), its own main section with relevant components, and a footer component that probably won't change much
+
 function App() {
   return (
-   <p>hello</p>
+   <BrowserRouter>
+   <Routes>
+    <Route path='/' element={<Landing />}>
+      <Route path='about_us' element={<AboutUs />} />
+      <Route path='jesses_story' element={<JessesStory />} />
+      <Route path='strep_a_awareness' element={<StrepAAwareness />} />
+      <Route path='contact_us' element={<ContactUs />} />
+      <Route path='donations' element={<Donations />} />
+      {/* TODO: Add a wildcard page that redirects to custom 404 screen */}
+    </Route>
+   </Routes>
+   </BrowserRouter>
   );
 }
 
