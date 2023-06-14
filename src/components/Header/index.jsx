07d569f4d939371e45b7d83776b645c998c75a89
HammontreeWebDev <hammontreewebdevelopment@gmail.com>
2023-06-14 19:13:32 -0400

    absolutely positioned the header shape instead of the nav bar, this allows more free design using flexbox for the text elements

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -4,10 +4,19 @@ import './assets/css/header.css';
 import headerShape from './assets/img/header-shape.svg';
 
 const Header = (props) => {
-    return(
-        <header className="header-component" style={{backgroundImage: `url(${props.backgroundImage})`}}>
-            <img className="header-shape" src={headerShape} />
-
+    return (
+        <header className="header-component" style={{ backgroundImage: `url(${props.backgroundImage})` }}>
+            <img className="header-shape" src={headerShape} alt="Red Arch to enhance design" />
+            <div className="header-content">
+                <h1 className="h1-header">Jesse Ryder Brown Foundation, INC</h1>
+                <nav className="nav-bar">
+                    <button>Hello</button>
+                    <button>Hello</button>
+                    <button>Hello</button>
+                    <button>Hello</button>
+                    <button>Hello</button>
+                </nav>
+            </div>
         </header>
     )
 }
