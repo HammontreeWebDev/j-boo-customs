97f138533ee07137ea281a8c1c918bc8720be468
HammontreeWebDev <hammontreewebdevelopment@gmail.com>
2023-06-13 23:02:43 -0400

    added initial set up of landing page

---
+++ src/pages/Landing/index.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+// Landing Page | route = /landing
+const Landing = () => {
+
+    return(
+        <h1>This is the Landing Page</h1>
+    )
+};
+
+export default Landing;
