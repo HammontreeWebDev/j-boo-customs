4242ba847f99c6ca88d9f09842713b79ea53532a
HammontreeWebDev <hammontreewebdevelopment@gmail.com>
2023-06-13 23:11:49 -0400

    All pages are imported into App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+// import all pages for react router to display properly
+import Landing from './pages/Landing';
+import AboutUs from './pages/AboutUs';
+import JessesStory from './pages/JessesStory';
+import StrepAAwareness from './pages/StrepAAwareness';
+import ContactUs from './pages/ContactUs';
+import Donations from './pages/Donations';
+
+// css file
+import './App.css';
+
+function App() {
+  return (
+   <p>hello</p>
+  );
+}
+
+export default App;
