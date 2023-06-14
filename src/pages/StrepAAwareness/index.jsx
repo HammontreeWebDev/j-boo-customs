ecf54abaf3358a3ff0679899b41c09221a55b083
HammontreeWebDev <hammontreewebdevelopment@gmail.com>
2023-06-13 23:04:16 -0400

    added initial set up of strep a awareness  page

---
+++ src/pages/StrepAAwareness/index.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+// Strep A Awareness Page | route = /strep_a_awareness
+const StrepAAwareness = () => {
+
+    return(
+        <h1>This is the Strep A Awareness Page</h1>
+    )
+};
+
+export default StrepAAwareness;
