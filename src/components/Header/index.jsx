84923094858a897756b413d2587e7e292a7a313e
HammontreeWebDev <hammontreewebdevelopment@gmail.com>
2023-06-14 20:58:25 -0400

    nav bar added, working on customized collapsible navbar

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,20 +1,35 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 import './assets/css/header.css';
 import headerShape from './assets/img/header-shape.svg';
 
 const Header = (props) => {
+    const [isNavCollapsed, setIsNavCollapsed] = useState(true);
+
+    useEffect(() => {
+        const handleResize = () => {
+            setIsNavCollapsed(window.innerWidth <= 1330);
+        };
+
+        window.addEventListener('resize', handleResize);
+
+        handleResize();
+
+        return () => window.removeEventListener('resize', handleResize);
+    }, []);
+
     return (
         <header className="header-component" style={{ backgroundImage: `url(${props.backgroundImage})` }}>
             <img className="header-shape" src={headerShape} alt="Red Arch to enhance design" />
             <div className="header-content">
                 <h1 className="h1-header">Jesse Ryder Brown Foundation, INC</h1>
-                <nav className="nav-bar">
-                    <button>Hello</button>
-                    <button>Hello</button>
-                    <button>Hello</button>
-                    <button>Hello</button>
-                    <button>Hello</button>
+                {/* <button className="toggle-nav-button" onClick={() => setIsNavCollapsed(!isNavCollapsed)}></button> */}
+                <nav className={`nav-bar ${isNavCollapsed ? 'collapsed' : ''}`}>
+                    <button className="subheading-text">About Us</button>
+                    <button className="subheading-text">Jesse's Story</button>
+                    <button className="subheading-text">Strep A Awareness</button>
+                    <button className="subheading-text">Contact Us</button>
+                    <button className="subheading-text">Donations</button>
                 </nav>
             </div>
         </header>
