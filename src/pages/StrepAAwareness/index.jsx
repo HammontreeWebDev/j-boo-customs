960d164d18235a817ca1f1deb51359890c038c34
HammontreeWebDev <hammontreewebdevelopment@gmail.com>
2023-06-14 23:44:34 -0400

    awareness header added

--- src/pages/StrepAAwareness/index.jsx
+++ src/pages/StrepAAwareness/index.jsx
@@ -1,10 +1,12 @@
 import React from "react";
+import Header from "../../components/Header";
+import awarenessHeader from './assets/img/awareness-header.png';
 
 // Strep A Awareness Page | route = /strep_a_awareness
 const StrepAAwareness = () => {
 
     return(
-        <h1>This is the Strep A Awareness Page</h1>
+        <Header backgroundImage={awarenessHeader} />
     )
 };
 
