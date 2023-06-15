a615a5f37764e7ad109704d6f8230cf909762f90
HammontreeWebDev <hammontreewebdevelopment@gmail.com>
2023-06-15 18:01:51 -0400

    responsiveness added to the about cards

--- src/components/FancyCard/index.jsx
+++ src/components/FancyCard/index.jsx
@@ -2,12 +2,24 @@ import React from "react";
 import './assets/css/fancy-card.css';
 import fancyCardHeader from './assets/img/fancy-card-header.svg';
 
-const FancyCard = () => {
+const FancyCard = (props) => {
 
     return(
         <div className="fancy-container">
             <img className="fancy-header" src={fancyCardHeader} alt="Green Arch to enhance design"/>
-            <h2 className="fancy-text"> Test text </h2>
+
+            {/* 30% width justify self center for picture*/}
+            <div className="title-pic-col fancy-text">
+                <h2 className="card-title reset-margin about-us-title">
+                    {props.memberName}
+                </h2>
+                <img className="member-photo" src={props.memberPhoto} alt={props.alt} />
+            </div>
+
+            {/* flex1 to take up space */}
+            <div className="text-col fancy-text">
+                <p>{props.aboutText}</p>
+            </div>
         </div>
     )
 }
--- src/pages/AboutUs/index.jsx
+++ src/pages/AboutUs/index.jsx
@@ -7,6 +7,9 @@ import PageTitle from "../../components/PageTitle";
 import ContentCard from "../../components/ContentCard";
 import FancyCard from "../../components/FancyCard";
 
+// import images
+import bradPlaceHolder from "./assets/img/brad-placeholder.png";
+
 // About Us Page located at /about_us
 const AboutUs = () => {
 
@@ -26,7 +29,12 @@ const AboutUs = () => {
                 
                 {/* About Me Cards */}
                 <section className="row">
-                    <FancyCard />
+                    <FancyCard memberName="Brad Brown | President" 
+                    memberPhoto={bradPlaceHolder} alt="Brad Brown | President"
+                    aboutText={<>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                    </>}/>
                 </section>
 
                 {/* <section className="row">
