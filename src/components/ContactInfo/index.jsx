54788c84a0449ec7a9a297f324ccd906f4bb501d
HammontreeWebDev <hammontreewebdevelopment@gmail.com>
2023-06-16 12:25:19 -0400

    changed layout for contact info

--- src/components/ContactInfo/index.jsx
+++ src/components/ContactInfo/index.jsx
@@ -1,16 +1,19 @@
 import React from "react";
 
+// css
+import './assets/css/contact-info.css';
+
 const ContactInfo = (props) => {
 
     return (
-        <>
+        <div className="information-container">
             <h2 className="subheading-text underline">{props.title}</h2>
             <p>
                 <span className="orange-text">{props.emailIcon}</span> {props.email}
                 <br></br>
                 <span className="orange-text">{props.phoneIcon}</span> {props.phone}
             </p>
-        </>
+        </div>
     )
 }
 
