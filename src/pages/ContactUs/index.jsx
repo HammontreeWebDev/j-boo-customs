e30b48c95726a6c121ee060112f91ec9395602b8
HammontreeWebDev <hammontreewebdevelopment@gmail.com>
2023-06-16 10:22:57 -0400

    re worked fancy cards to be more responsive via flexbox

--- src/pages/ContactUs/index.jsx
+++ src/pages/ContactUs/index.jsx
@@ -1,9 +1,13 @@
 import React from "react";
+
+// import images
 import contactUsHeader from './assets/img/contact-us-header.png';
+import contactPh from './assets/img/contact-ph.png';
 
 // import components
 import Header from "../../components/Header";
 import PageTitle from "../../components/PageTitle";
+import FancyCard from "../../components/FancyCard";
 
 // Contact Us Page | route = /contact_us
 const ContactUs = () => {
@@ -12,6 +16,13 @@ const ContactUs = () => {
         <>
         <Header backgroundImage={contactUsHeader} />
         <PageTitle whiteText="Contact" greenText="Us"/>
+        <main>
+
+            <section className="row">
+            <FancyCard memberName="Contact Information" memberPhoto={contactPh} alt="white envelope with a red pencil"/>
+            </section>
+
+        </main>
         </>
     )
 };
