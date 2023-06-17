05b033bbdcaaff9032c2bfe07f4e780f4f02a44a
HammontreeWebDev <hammontreewebdevelopment@gmail.com>
2023-06-17 16:39:42 -0400

    jesses story page transitions and loading spinners added

--- src/components/Jesse/index.jsx
+++ src/components/Jesse/index.jsx
@@ -1,15 +1,28 @@
 import React from "react";
+import useImageLoading from '../../utils/imageLoadingUtils';
+// css
 import './assets/css/jesse.css';
+// components
+import LoadSpinner from "../LoadSpinner";
 
 const Jesse = (props) => {
+    // keep track of state of image load using helper function
+    const { isImageLoaded, handleImageLoad } = useImageLoading();
+    // ! For Images
+    const imgClass = isImageLoaded ? "jesse-img animate__animated animate__fadeIn" : "hidden";
+
+    // ! For Entire Div
+    const initialClass = "jesse-container animate__animated animate__fadeIn";
+    const exitClass = "jesse-container animate__animated animate__fadeOut";
 
     return (
-        <div className="jesse-container">
+        <div className={props.isPageChanging ? exitClass : initialClass}>
             <div className="title-container">
                 <div className="h1-header jesse-title">
                     {props.whiteTitle} <span className="light-blue-text">{props.blueTitle}</span>
                 </div>
-                <img className="jesse-img" src={props.src} alt={props.alt} />
+                <img className={imgClass} src={props.src} alt={props.alt} onLoad={handleImageLoad} onLoadedData={handleImageLoad} />
+                {isImageLoaded ? null : <LoadSpinner />}
             </div>
 
             <div className="text-container">
