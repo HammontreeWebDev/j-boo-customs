834eff00ad0016789f26480706e81415a58777a7
HammontreeWebDev <hammontreewebdevelopment@gmail.com>
2023-06-16 21:29:12 -0400

    completed footer

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -10,6 +10,8 @@ const Footer = () => {
     return(
         <footer className="footer-component">
             <img className="footer-shape" src={footerShape} alt="Red Arch to enhance design" />
+            <p className="copyright">Copyright © 2023 Jesse Ryder Brown Foundation, Inc.</p>
+            <small className="small-text">Website Built By: <span><a className="hyperlink" href="https://www.kailebhammontree.com/" target="_blank">Hammontree Full-Stack Solutions LLC</a></span></small>
         </footer>
     )
 }
