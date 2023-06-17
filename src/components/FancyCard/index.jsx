dc684253d646d31dfe798c57604271bae1cf23fb
HammontreeWebDev <hammontreewebdevelopment@gmail.com>
2023-06-17 16:08:37 -0400

    about us loading and transitions are completed

--- src/components/FancyCard/index.jsx
+++ src/components/FancyCard/index.jsx
@@ -1,18 +1,32 @@
 import React from "react";
+import useImageLoading from '../../utils/imageLoadingUtils';
+// css
 import './assets/css/fancy-card.css';
 import fancyCardHeader from './assets/img/fancy-card-header.svg';
 
-const FancyCard = (props) => {
+// components
+import LoadSpinner from "../LoadSpinner";
 
-    return(
-        <div id={props.fancyContainerId} className="fancy-container">
-            <img className="fancy-header" src={fancyCardHeader} alt="Green Arch to enhance design"/>
+const FancyCard = (props) => {
+    // keep track of state of image load using helper function
+    const { isImageLoaded, handleImageLoad } = useImageLoading();
+    // ! For Images
+    const imgClass = isImageLoaded ? "member-photo animate__animated animate__fadeIn" : "hidden";
+
+    // ! For Entire Div
+    const initialClass = "fancy-container animate__animated animate__fadeIn";
+    const exitClass = "fancy-container animate__animated animate__fadeOut";
+
+    return (
+        <div id={props.fancyContainerId} className={props.isPageChanging ? exitClass : initialClass}>
+            <img className="fancy-header" src={fancyCardHeader} alt="Green Arch to enhance design" />
 
             <div id={props.titleColId} className="title-pic-col fancy-text">
                 <h2 className="card-title reset-margin about-us-title">
                     {props.memberName}
                 </h2>
-                <img id={props.photoId} className="member-photo" src={props.memberPhoto} alt={props.alt} />
+                <img id={props.photoId} className={imgClass} src={props.memberPhoto} alt={props.alt} onLoad={handleImageLoad} onLoadedData={handleImageLoad} />
+                {isImageLoaded ? null : <LoadSpinner />}
             </div>
 
             <div className={props.classSelector}>
--- src/pages/AboutUs/index.jsx
+++ src/pages/AboutUs/index.jsx
@@ -3,7 +3,6 @@ import useNavigation from "../../utils/navigationUtils";
 
 // import components
 import Header from "../../components/Header";
-import aboutUsHeader from './assets/img/about-us-header.png';
 import PageTitle from "../../components/PageTitle";
 import ContentCard from "../../components/ContentCard";
 import FancyCard from "../../components/FancyCard";
@@ -11,6 +10,7 @@ import Footer from "../../components/Footer";
 
 // import images
 import bradPlaceHolder from "./assets/img/brad-placeholder.png";
+import aboutUsHeader from './assets/img/about-us-header.png';
 
 // About Us Page located at /about_us
 const AboutUs = () => {
@@ -33,7 +33,7 @@ const AboutUs = () => {
 
                 {/* About Me Cards */}
                 <section className="row">
-                    <FancyCard classSelector="text-col fancy-text" memberName="Brad Brown | President"
+                    <FancyCard isPageChanging={isPageChanging} classSelector="text-col fancy-text" memberName="Brad Brown | President"
                         memberPhoto={bradPlaceHolder} alt="Brad Brown | President"
                         aboutText={<>
                             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
